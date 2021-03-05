addb0523c606d6656ef3cec560e160a421326fbd
Taylor Treece <taylor.treece@protonmail.com>
2021-03-05 11:08:14 -0600

    Done.

---
+++ app/models/dog.rb
@@ -0,0 +1,21 @@
+class Dog < Sinatra::Base
+
+    attr_accessor :name, :breed, :age
+
+    @@all=[]
+
+    def initialize(name, breed, age)
+        @name=name
+        @breed=breed
+        @age=age
+        self.save
+    end
+
+    def save
+        @@all<<self
+    end
+
+    def self.all
+        @@all
+    end
+end
